293017bb0d87ac945338fcfa72c6f470782211cf
Ankit <ankit141003@gmail.com>
2023-09-17 01:48:18 +0530

    added user authentiaction and website

--- app.py
+++ app.py
@@ -1,11 +1,59 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash
+from firebase_db import new_email_ckeck, append_to_existing_emails, check_password
 
 app = Flask(__name__)
 app.secret_key = 'db33edb0296bf8f4737c321c82d2103e933f333d50fb3ae9f1758002c3e0dc79'
 
-@app.route("/", method=['GET', 'POST'])
-def landing():
-    return render_template("login.html")
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    message = ''
+    if request.method=='POST':
+        email = request.form['email']
+        password = request.form['password']
+        if new_email_ckeck(email):
+            message = '<h4 style="color: red;">Invalid Email</h4>'
+            return render_template("login.html", message=message)
+        else:
+            if check_password(email, password):
+                session['user_email'] = email
+                return redirect(url_for("home"))
+            else:
+                message = '<h4 style="color: red;">Invalid Passsword</h4>'
+                return render_template("login.html", message=message)
+    else:
+        return render_template("login.html", message=message)
+    
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    # if request.method=='POST':
+    #     message = '<a style="color: red;">Invalid Credentials<a>'
+    #     return render_template("login.html", message=message)
+    # else:
+        message = ''
+        return render_template("login.html", message=message)
+
+
+
+@app.route("/home")
+def logout():
+    if 'user_email' not in session:
+        return redirect(url_for("/"))
+    else:
+        return redirect(url_for("home"))
+
+
+@app.route("/about")
+def about():
+    return render_template("about_us")
+
+@app.route("/contact_us")
+def contact():
+    return render_template("contact_us") 
+
+@app.route("/sign_up")
+def signup():
+    return render_template("sign_up") 
+
 
 
 if __name__ == '__main__':
--- templates/index.html
+++
@@ -1,1 +0,0 @@
-<h1>Hellooooo</h1>
